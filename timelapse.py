2e4543167f33b549aaba625a852449bf81a53c0e
Mikkel Jeppesen <2756925+Duckle29@users.noreply.github.com>
2018-10-02 16:18:08 +0200

    Converted to tabs

--- timelapse.py
+++ timelapse.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 
-# pylint: disable-msg=C0103
-
 from os import listdir
 from os.path import isfile, join, splitext
 import datetime
 import subprocess
 import math
 from optparse import OptionParser, OptionGroup
+import sys
 from PIL import Image
-
 
 opt_parser = OptionParser()
 
@@ -17,66 +15,71 @@ opt_parser = OptionParser()
 
 timing_options = OptionGroup(opt_parser, "Timing options")
 
-timing_options.add_option("-p", "--period",
-                          type="int", default=0,
-                          action="store", dest="period",
-                          help="Period between images in seconds")
+timing_options.add_option(
+	"-p", "--period",
+	type="int", default=0,
+	action="store", dest="period",
+	help="Period between images in seconds")
 
-timing_options.add_option("-d", "--duration",
-                          type="int", default=0,
-                          action="store", dest="duration",
-                          help="Duration in seconds for the timelapser to run")
+timing_options.add_option(
+	"-d", "--duration",
+	type="int", default=0,
+	action="store", dest="duration",
+	help="Duration in seconds for the timelapser to run")
 
-timing_options.add_option("-r", "--framerate",
-                          type="int", default=10,
-                          action="store", dest="framerate",
-                          help="User specified framerate of the finished timelapse")
+timing_options.add_option(
+	"-r", "--framerate",
+	type="int", default=10,
+	action="store", dest="framerate",
+	help="User specified framerate of the finished timelapse")
 
 opt_parser.add_option_group(timing_options)
 
-
-
 storage_options = OptionGroup(opt_parser, "Storage options")
-storage_options.add_option("-f", "--infolder",
-                           default="images",
-                           action="store", dest="in_folder",
-                           help="Folder to store images in")
+storage_options.add_option(
+	"-f", "--infolder",
+	default="images",
+	action="store", dest="in_folder",
+	help="Folder to store images in")
 
-storage_options.add_option("-F", "--outfolder",
-                           default="output",
-                           action="store", dest="out_folder",
-                           help="Folder to store images in")
+storage_options.add_option(
+	"-F", "--outfolder",
+	default="output",
+	action="store", dest="out_folder",
+	help="Folder to store images in")
 
-storage_options.add_option("-t", "--type",
-                           default=".png",
-                           action="append", dest="allowed_types",
-                           help="Add allowed file types. Mostly intended for use without integrated image capture")
+storage_options.add_option(
+	"-t", "--type",
+	default=".jpg",
+	action="append", dest="allowed_types",
+	help="Add allowed file types. Mostly intended for use without integrated image capture")
 opt_parser.add_option_group(storage_options)
-
-
 
 post_processing_options = OptionGroup(opt_parser, "Post Processing options")
 
-post_processing_options.add_option("-R", "--rotate",
-                                   type="int", default= 0,
-                                   action="store", dest="rotate",
-                                   help="Degrees to rotate image. Currently only 180 or 0 Deg is supported")
+post_processing_options.add_option(
+	"-R", "--rotate",
+	type="int", default=0,
+	action="store", dest="rotate",
+	help="Degrees to rotate image. Currently only 180 or 0 Deg is supported")
 
-post_processing_options.add_option("-c", "--crop",
-                                   default=False,
-                                   action="store_true", dest="crop",
-                                   help="Crop image to supplied ratio requires ratio to be provided")
-post_processing_options.add_option("-a", "--ratio",
-                                   type="float", default=0,
-                                   action="append", dest="ratio",
-                                   help="Desired output ratio, Ex: -a 16 -a 9 will be 16:9")
-post_processing_options.add_option("-s", "--scale",
-                                   default=False,
-                                   action="store_true", dest="scale",
-                                   help="Scale image to size. If only one dimention is given it'll retain ratio. Ex: -s 1920 -s 1080")
+post_processing_options.add_option(
+	"-c", "--crop",
+	default=False,
+	action="store_true", dest="crop",
+	help="Crop image to supplied ratio requires ratio to be provided")
+post_processing_options.add_option(
+	"-a", "--ratio",
+	type="float", default=0,
+	action="append", dest="ratio",
+	help="Desired output ratio, Ex: -a 16 -a 9 will be 16:9")
+post_processing_options.add_option(
+	"-s", "--scale",
+	default=0,
+	action="append", dest="scale",
+	help="Scale image to size. If only width is given it'll retain ratio. Ex: -s 1920 -s 1080")
 
 opt_parser.add_option_group(post_processing_options)
-
 
 (options, args) = opt_parser.parse_args()
 
@@ -87,82 +90,104 @@ opt_parser.add_option_group(post_processing_options)
 # It's case sensitive
 image_name_pattern = 'img%05d.jpg'
 
-#Lazily using a timestamp to name the output. Should be pretty much entirely unique
+# Lazily using a timestamp to name the output. Should be pretty much entirely unique
 video_name = 'Timelapse-{:%Y-%m-%d %H_%M_%S}'.format(datetime.datetime.now())
 
 ## CONFIG END
+
+
+
+
+# Processing configs here
 
 # Rotate the images 180 degrees?
 rotate = False
 
 if options.rotate != 0:
-    rotate = True
-
-
-# Processing configs here
+	rotate = True
 
 # Get a list of all images
-images = [f for f in listdir(image_path) if isfile(join(image_path, f))]
-for item in images:
-    allowed = False
-    fn, ext = splitext(item)
-    for allowedExt in allowedFileExtensions:
-        if ext.lower() == allowedExt.lower():
-            allowed = True
-    if not allowed:
-        images.remove(item)
+
+def get_images(path, file_types):
+	"This returns a list of all images of allowed type in the path"
+	# print("Getting images from {} of type {}".format(path, ','.join(file_types)))
+	print("Getting images from {}".format(path))
+
+	# List all files and remove files that aren't images from the list
+	images = [f for f in listdir(path) if isfile(join(path, f))]
+
+	for item in images:
+		file_name, ext = splitext(item)
+		if ext.lower() not in [x.lower() for x in file_types]:
+			images.remove(item)
+
+	return images
+
+
+images = get_images(options.in_folder, options.allowed_types)
 
 img_count = len(images)
 
 # Transformations
-if crop:
-    img = Image.open(image_path + '/' + images[0])
-    width, height = img.size
-    img.close()
+crop_argument = ''
+scale_argument = ''
+flip_argument = ''
+if options.crop:
 
-    cropped_height = math.floor(width * crop_ratio[1]/crop_ratio[0])
-    crop_height_start = math.floor((height - cropped_height)/2)
+	if options.ratio.len() != 2:
+		print('Invalid aspect ratio provided')
+		sys.exit(1)
 
-    crop_argument = 'crop={}:{}:0:{}'.format(width,cropped_height,crop_height_start)
+	# Get the dimensions of the first image.
+	img = Image.open(options.in_folder + '/' + images[0])
+	width, height = img.size
+	img.close()
 
-if scale:
-    scale_argument = 'scale={}:-1'.format(scale_width)
+	cropped_height = math.floor(width * options.ratio[1] / options.ratio[0])
+	crop_height_start = math.floor((height - cropped_height) / 2)
+
+	crop_argument = 'crop={}:{}:0:{}'.format(width, cropped_height, crop_height_start)
+
+if options.scale > 0:
+	if options.scale.len() > 2:
+		print('Invalid scale')
+		sys.exit(2)
+	elif options.scale.len() == 2:
+		scale_argument = 'scale={}:{}'.format(options.scale[0], options.scale[1])
+	else:
+		scale_argument = 'scale={}:-1'.format(options.scale[0])
+
 
 if rotate:
-    flip_argument = 'hflip,vflip'
+	flip_argument = 'hflip,vflip'
 
 # construct transformation argument
 first = True
-if crop:
-    first = False
-    transformation_argument = crop_argument
 
-if scale:
-    if not first:
-        transformation_argument += ','
+transformation_argument = []
 
-    first = False
-    transformation_argument += scale_argument
+if options.crop:
+	transformation_argument.append(crop_argument)
+
+if options.scale:
+	transformation_argument.append(scale_argument)
 
 if rotate:
-    if not first:
-        transformation_argument += ','
-
-    first = False
-    transformation_argument += flip_argument
+	transformation_argument.append(flip_argument)
 
 # Transformations end
 
 if framerate == 0:
-    framerate = math.floor(img_count / desired_video_duration)+1 # +1 to avoid a framerate of 0fps
+	framerate = math.floor(img_count / desired_video_duration) + 1  # +1 to avoid a framerate of 0fps
 
 # Construct subprocess list
 
-command = ['ffmpeg', '-r', '{}'.format(framerate), '-f', 'image2', '-start_number', start_number, '-i', image_path + '/' + image_name_pattern, '-codec:v', 'libx264'] 
+command = ['ffmpeg', '-r', '{}'.format(framerate), '-f', 'image2', '-start_number', start_number, '-i',
+		   image_path + '/' + image_name_pattern, '-codec:v', 'libx264']
 
 if crop or scale or rotate:
-    command.append('-vf')
-    command.append(transformation_argument)
+	command.append('-vf')
+	command.append(transformation_argument)
 
 command.append(video_name + '.mp4')
 
