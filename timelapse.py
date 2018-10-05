c1fdbcd190861e5c78f6070d9cd57f6f0a9320e1
Mikkel Jeppesen <2756925+Duckle29@users.noreply.github.com>
2018-10-05 02:31:25 +0200

    Did stuff

--- timelapse.py
+++ timelapse.py
@@ -264,8 +264,9 @@ def image_handling(duration, period, local_path, name_pattern, allowed_types, re
 			last_image += 1
 			subprocess.run(command)
 
-		elif not bool(last_image % 10):  # For every 10 images, move them to the remote
+		elif last_image % 10 == 0:  # For every 10 images, move them to the remote
 			imgs = get_images(local_path, allowed_types)
+
 			put_images(imgs, local_path, remote['folder'], remote, True)
 
 		else:
